424dcd52455933c99af1a296bb3b6368ba0d32c6
Ardaas1k <Ardaas1k@users.noreply.github.com>
2020-03-16 19:52:59 +0300

    MainCharacterTest
    Move2D script added.
    Jump script added.
    Double Jump added.
    Grounded script added.

---
+++ VrakItDown/Assets/MainCharacterTest/Scripts/Move2D.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move2D : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    public bool isGrounded = false;
+    public bool canDoubleJump = false;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            canDoubleJump = true;
+        }
+        else if (Input.GetButtonDown("Jump") && canDoubleJump == true)
+        {
+            canDoubleJump = false;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+
+        }
+
+    }
+
+    
+}
