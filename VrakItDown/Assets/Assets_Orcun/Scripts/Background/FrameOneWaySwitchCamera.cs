301ab105880584e7e7d9f312aeb2368825b48ce4
Qrow97 <60518921+Qrow97@users.noreply.github.com>
2020-04-10 23:47:08 +0300

    Level done
    Level done, added new script for boss frame, not allowed to go back previous frame

---
+++ VrakItDown/Assets/Assets_Orcun/Scripts/Background/FrameOneWaySwitchCamera.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameOneWaySwitchCamera : MonoBehaviour
+{
+    public GameObject currentFrame;
+    public GameObject nextFrame;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (currentFrame.active == true)
+        {
+            currentFrame.SetActive(false);
+            nextFrame.SetActive(true);
+           
+           // transform.GetComponent<BoxCollider2D>().isTrigger = false;
+        }
+      
+    }
+
+   
+}
